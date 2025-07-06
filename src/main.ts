f03febac7791d368bcc4cf50622dc461e9ed1844
faizakailani <faizalani04@gmail.com>
2025-07-06 22:54:57 +0700

    new readme file

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  //app listen
   await app.listen(3000);
 }
 bootstrap();
